ef03550232993f14cb90034f7258832e72a1806e
Pawash <ahujapawash@gmail.com>
2016-11-15 19:09:02 +0000

    First commit

---
+++ impl/src/main/java/org/nokia/pahuja/impl/Controller.java
@@ -0,0 +1,49 @@
+package org.nokia.pahuja.impl;
+
+
+import java.util.List;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Controller implements DataChangeListener, AutoCloseable, PacketProcessingListener{
+
+
+	private List<Registration> registrations;
+	private DataBroker databroker;
+	private PacketProcessingService packetProcessingService;
+
+	public Controller() {
+		// TODO Auto-generated constructor stub
+		System.out.println("inside L2 forwarding");
+		//this.packetProcessingService = rpcProviderRegistry.getRpcService(PacketProcessingService.class);
+	}
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("data changed");
+
+	}
+
+	@Override
+	public void onPacketReceived(PacketReceived notification) {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ impl/src/main/java/org/nokia/pahuja/impl/PahujaProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2015 Nokia, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.nokia.pahuja.impl;
+
+import java.util.List;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.controller.sal.binding.api.NotificationService;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.NotificationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.opendaylight.yangtools.concepts.Registration;
+public class PahujaProvider implements BindingAwareProvider, AutoCloseable, PacketProcessingListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PahujaProvider.class);
+    private ListenerRegistration<NotificationListener> listener;
+    private List<Registration> registrations;
+//    public PahujaProvider(NotificationProviderService notificationProviderService){
+
+  //  }
+
+
+	public PahujaProvider(@SuppressWarnings("deprecation") NotificationProviderService notificationProviderService) {
+		// TODO Auto-generated constructor stub
+		listener = notificationProviderService.registerNotificationListener(this);
+		registrations.add(listener);
+
+	}
+	@Override
+    public void onSessionInitiated(ProviderContext session) {
+        LOG.info("PahujaProvider Session Initiated");
+        System.out.println("Hiiiiiiii!");
+//      DataBroker db = session.getSALService(DataBroker.class);
+
+
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("PahujaProvider Closed");
+        System.out.println("Bye");
+        this.listener.close();
+
+    }
+    @Override
+    public  void onPacketReceived(PacketReceived packet) {
+	LOG.debug("reveived the packet :" + packet.toString());
+        System.out.println("packet received");
+
+
+	}
+}
